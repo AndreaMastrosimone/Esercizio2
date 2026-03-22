873ebc187ffb1df85390e87fe6fe4632fd3e93f4
aaaaaaaaaa <ANDREABENEDETTO.MASTROSIMONE@studenti.units.it>
2026-03-23 00:50:16 +0100

    Refactor character counting and text manipulation logic

--- esercizio2.py
+++ esercizio2.py
@@ -36,12 +36,57 @@ print('Il totale delle parole è',len(parole))
 alfanumerici="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
 caratteri=0
 for i in testo:
-  for valido in alfanumerici:
+  for valido in alfanumerici: ##alterantiva: usare direttamente for i in alfanumerici
     if i==valido:
       caratteri=caratteri+1
 print('Il numero di caratteri è: ',caratteri)
 
 '''Chiedere all’utente una lettera e contate quante volte compare nel testo'''
-print('Scegli una lettera')
+alfabeto="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+letter=input('Scegli una lettera')
+if letter not in alfabeto:
+  letter=input('Carattere non valido. Selezionare una lettera')
+print('conteggio: ',testo.count(letter))
+
+'''Sostituite tutte le parole day, water e about con la parola PYTHON in tutti i versi'''
+new_testo=[]
+for i in parole:
+  if i.lower() not in ["day","water","about"]:
+    new_testo.append(i)
+  else :
+    new_testo.append("PYTHON")
+
+'''Riscrivete il testo in modo che tutte le parole in posizione dispari siano scritte in maiuscolo'''
+testo_upper=[]
+j=0
+for i in parole:
+  j=j+1
+  if j%2!=0:
+    testo_upper.append(i.upper())
+  else:
+    testo_upper.append(i)
+
+'''Riscrivere il testo invertendo l’ordine delle frasi dal basso all’alto.'''
+testo_contrario=[]
+for i in range(len(diviso)-1,-1,-1): ##primo -1 perchè vogliamo includere lo 0, secondo -1 per decrementare
+  testo_contrario.append(diviso[i])
+
+'''Riscrivete il testo in modo che il secondo verso di ogni strofa sia scritto a specchio (cioè al contrario carattere per carattere: Ad esempio, questa frase’ –> ‘esarf atseuq ,oipmese dA’)'''
+testo_modificato=[]
+for i in range( len(diviso)):
+  if i%4==1:
+    testo_modificato.append(diviso[i][::-1]) ##slicing negativo
+  else:
+    testo_modificato.append(diviso[i])
+  
+'''Trovate eventuali parole che compaiono in tutte le strofe'''
+strofe=testo.split("\n\n")
+parole_strofe=strofe.split()
+set_parole_strofe={parole_strofe
+                }
+    
+
+  
+
 
 
